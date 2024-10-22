7ba15999875c4ca9807d692f914804189cc40db5
OrozcoFidel4 <fidel21304022@gmail.com>
2024-10-22 14:10:55 -0700

    Consumo De API en ReactNative

---
+++ ConsumoAPI/ConsumirAPI/App.js
@@ -0,0 +1,65 @@
+import React from 'react';
+import {ScrollView, FlatList, StyleSheet, Text, View } from 'react-native';
+
+
+
+
+export default class App extends React.Component{
+  
+  constructor(props){
+    super(props);
+
+    this.state={
+      loading: false,
+      pokemon: [],
+      url: 'https://pokeapi.co/api/v2/pokemon/'
+
+    }
+  }
+
+  componentDidMount(){
+    this.getPokemon()
+  }
+
+  getPokemon = () => {
+
+    this.setState({loading: true})
+
+    fetch(this.state.url)
+    .then(res => res.json())
+    .then(res => {
+      
+      this.setState({
+        pokemon: res.results,
+        url: res.next,
+        loading: false
+      })
+    });
+  }; 
+
+render(){
+    return (
+      <>
+      <ScrollView>
+        <View style={styles.container}>
+          <Text style={{marginTop: 80, marginBottom: 20, fontWeight: 'bold', fontSize:30}}>Consumir API</Text>
+          
+          <FlatList
+          data= {this.state.pokemon}
+          renderItem={({item}) => <Text> {item.name} </Text>
+          }
+          /> 
+        </View>
+      </ScrollView>
+      </>
+    );
+    }
+}
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
